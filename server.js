7bfdc664d0e48c67af569cfffe0b63e615e77552
hsurya <hasan_surya@yahoo.com>
2017-09-18 19:05:39 +0700

    coba

--- server.js
+++ server.js
@@ -1,9 +1,4 @@
 //  OpenShift sample Node application
-var express = require('express'),
-    app     = express(),
-    morgan  = require('morgan');
-    
-Object.assign=require('object-assign')
 
   
   var enableCORS = function(req, res, next) {
@@ -19,8 +14,22 @@ Object.assign=require('object-assign')
 
 app.use(enableCORS);
 
+var express = require('express'),
+    app     = express(),
+    morgan  = require('morgan');
+    
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+io.on('connection', function(socket){
+	console.log('socket connected bla bla bla');
+
+});
+
+Object.assign=require('object-assign');
+
 app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
